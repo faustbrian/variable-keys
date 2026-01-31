51653091385881cda9303070079fe9ff8c316692
Brian Faust <brian@cline.sh>
2026-01-31 10:34:41 +0200

    first commit
    Signed-off-by: Brian Faust <brian@cline.sh>

---
+++ src/Exceptions/CannotAssignNonStringToUlidException.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) Brian Faust
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cline\VariableKeys\Exceptions;
+
+use RuntimeException;
+
+use function get_debug_type;
+use function sprintf;
+
+final class CannotAssignNonStringToUlidException extends RuntimeException
+{
+    public static function forValue(mixed $value): self
+    {
+        $type = get_debug_type($value);
+
+        return new self(
+            sprintf('Cannot assign non-string value of type [%s] to ULID primary key. ', $type).
+            'ULID primary keys must be strings.',
+        );
+    }
+}
